c7b65559838d84cb3e9a738bd89157364be85d89
pdyby <patrick.byrne@mckeon.ie>
2024-04-24 15:49:49 +0100

    Final remove

--- src/__test__/city.test.ts
+++ src/__test__/city.test.ts
@@ -4,36 +4,6 @@ import { ICity } from '../interface';
 
 const executeAllTests = (City: any) => {
 	describe('Check for City Module', () => {
-		test('Check Cities for Delhi', () => {
-			const countryCode = 'IN';
-			const stateCode = 'DL';
-			const cities: any = City.getCitiesOfState(countryCode, stateCode);
-			const names = cities.map((city: ICity) => {
-				return city.name;
-			});
-			expect(names).toEqual([
-				'Alipur',
-				'Bawana',
-				'Central Delhi',
-				'Delhi',
-				'Deoli',
-				'East Delhi',
-				'Karol Bagh',
-				'Najafgarh',
-				'Nangloi Jat',
-				'Narela',
-				'New Delhi',
-				'North Delhi',
-				'North East Delhi',
-				'North West Delhi',
-				'Pitampura',
-				'Rohini',
-				'South Delhi',
-				'South West Delhi',
-				'West Delhi',
-			]);
-		});
-
 		test('Check Cities for undefined State', () => {
 			let countryCode;
 			const stateCode = 'DL';
--- src/__test__/state.test.ts
+++ src/__test__/state.test.ts
@@ -4,32 +4,6 @@ import { IState } from '../interface';
 
 const executeAllTests = (State: any) => {
 	describe('Check for State Module', () => {
-		test('Check for Canada States', () => {
-			const code = 'CA';
-			const country: any = Country.getCountryByCode(code);
-			const states: IState[] = State.getStatesOfCountry(country.isoCode);
-			const names: String[] = states.map(
-				(state: IState): String => {
-					return state.name;
-				},
-			);
-			expect(names).toEqual([
-				'Alberta',
-				'British Columbia',
-				'Manitoba',
-				'New Brunswick',
-				'Newfoundland and Labrador',
-				'Northwest Territories',
-				'Nova Scotia',
-				'Nunavut',
-				'Ontario',
-				'Prince Edward Island',
-				'Quebec',
-				'Saskatchewan',
-				'Yukon',
-			]);
-		});
-
 		test('Check All States for United States Of America', () => {
 			const code = 'US';
 			const country: any = Country.getCountryByCode(code);
@@ -106,56 +80,6 @@ const executeAllTests = (State: any) => {
 				'West Virginia',
 				'Wisconsin',
 				'Wyoming',
-			]);
-		});
-
-		test('Check States for India', () => {
-			const code = 'IN';
-			const country: any = Country.getCountryByCode(code);
-			const states: IState[] = State.getStatesOfCountry(country.isoCode);
-			const names: String[] = states.map(
-				(state: IState): String => {
-					return state.name;
-				},
-			);
-
-			expect(names).toEqual([
-				'Andaman and Nicobar Islands',
-				'Andhra Pradesh',
-				'Arunachal Pradesh',
-				'Assam',
-				'Bihar',
-				'Chandigarh',
-				'Chhattisgarh',
-				'Dadra and Nagar Haveli and Daman and Diu',
-				'Delhi',
-				'Goa',
-				'Gujarat',
-				'Haryana',
-				'Himachal Pradesh',
-				'Jammu and Kashmir',
-				'Jharkhand',
-				'Karnataka',
-				'Kerala',
-				'Ladakh',
-				'Lakshadweep',
-				'Madhya Pradesh',
-				'Maharashtra',
-				'Manipur',
-				'Meghalaya',
-				'Mizoram',
-				'Nagaland',
-				'Odisha',
-				'Puducherry',
-				'Punjab',
-				'Rajasthan',
-				'Sikkim',
-				'Tamil Nadu',
-				'Telangana',
-				'Tripura',
-				'Uttar Pradesh',
-				'Uttarakhand',
-				'West Bengal',
 			]);
 		});
 
